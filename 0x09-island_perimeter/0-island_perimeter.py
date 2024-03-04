68b9a7298ded7ec8bbb1e47e1286f84a60d88665
div-styl <gouhmada@gmail.com>
2024-03-04 12:05:07 +0100

    alx challenge

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines island perimeter finding func"""
+
+
+def island_perimeter(grid):
+    """ return the perimeter"""
+    w = len(grid[0])
+    h = len(grid)
+    e = 0
+    s = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                s += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    e += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    e += 1
+    return s * 4 - e * 2
