0c140beefb7ed2c93c4dc1c9aad6f1864ac6a2cd
div-styl <gouhmada@gmail.com>
2024-03-12 13:14:26 +0000

    project is finished

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    define function
+"""
+
+def primes(n):
+    """ returns the list of prime numbers """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
+def isWinner(x, nums):
+    """
+        returns name of winners maria or ben or none if no winner
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
